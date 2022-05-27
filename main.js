b1069f74f88b3f63db6c809c09eb31f9628ab56f
Sebastian Alfaro <sebas43243@hotmail.com>
2022-05-27 02:31:06 -0500

    added imgs to call to action

--- main.js
+++ main.js
@@ -2,48 +2,47 @@ const selectElement = function (element) {
     return document.querySelector(element);
 };
 
-let menuToggler=selectElement('.menu-toggle');
-let body=selectElement('body');
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
 
-menuToggler.addEventListener('click', function() {
+menuToggler.addEventListener('click', function () {
     body.classList.toggle('open');
 });
 
-menuToggler.addEventListener('blur',function(){
+menuToggler.addEventListener('blur', function () {
     body.classList.toggle('close');
 });
 
-window.sr=ScrollReveal();
+window.sr = ScrollReveal();
 
-sr.reveal('.animate-left',{
-    origin:'left',
-    duration:1000,
-    distance:'25rem',
-    delay:300
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
 });
 
-sr.reveal('.animate-right',{
-    origin:'right',
-    duration:1000,
-    distance:'25rem',
-    delay:600
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
 });
 
-sr.reveal('.animate-top',{
-    origin:'top',
-    duration:1000,
-    distance:'25rem',
-    delay:600
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
 });
 
-sr.reveal('.animate-bottom',{
-    origin:'bottom',
-    duration:1000,
-    distance:'25rem',
-    delay:600
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
 });
-function changeClass()
-{
+function changeClass() {
 
-    document.getElementById('body').className="";
+    document.getElementById('body').className = "";
 }
